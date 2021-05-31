2d12fffe15eff59a48dc48b00234ea7302143d16
WdvOps <dvops@outlook.com>
2021-05-31 16:28:52 -0300

    feat: Settings of the data biding, and integration wtih the data base

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
@@ -13,7 +14,8 @@ import { StockCardComponent } from './stock-card/stock-card.component';
 
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
   ],
   exports: [
 
